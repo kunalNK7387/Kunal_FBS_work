262eb91211794a53bf7500155079bda46f950b66
kunalNK7387 <kunalkhairnar595@gmail.com>
2025-07-27 20:06:08 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignment 7/Function Type 3/A4/Q3.perfectno.c
@@ -0,0 +1,26 @@
+void perfectNumber(int*);
+void main() {
+	int num;
+
+	printf("Enter range: ");
+	scanf("%d", &num);
+	
+	perfectNumber(&num);
+}
+void perfectNumber(int* num){
+
+	
+	for (int i = 1; i <= *num; i++) {
+		int sum = 0;
+
+		for (int j = 1; j < i; j++) {
+			if (i % j == 0) {
+				sum += j;
+			}
+		}
+
+		if (sum == i) {
+			printf("%d ", i);
+		}
+	}
+}
