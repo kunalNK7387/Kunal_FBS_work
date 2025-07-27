d80be371d267ad7343ee67eb723b0badd55f20f0
kunalNK7387 <kunalkhairnar595@gmail.com>
2025-07-27 19:39:27 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignment 6/Type 1 function/A4/Q1.armsrong.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+void Armstrong();
+int p(int ,int);
+int main() {
+	Armstrong();
+}
+void Armstrong(){
+
+    int n;
+
+    printf("Enter the upper limit (n): ");
+    scanf("%d", &n);
+
+    printf("Armstrong numbers between 1 and %d are:\n", n);
+
+    for (int i = 1; i <= n; i++) {
+        int num = i;
+        int sum = 0;
+        int digits = 0;
+        int temp = num;
+
+        while (temp > 0) {
+            digits++;
+            temp /= 10;
+        }
+
+        temp = num;
+
+        while(temp > 0){
+		int rem = temp % 10;
+		sum = sum + pow(rem,digits);
+		temp /=10;
+	}
+        if (sum == num) {
+            printf("%d ", num);
+        }
+    }
+}
+int p(int base,int expo){
+	int res=1;
+	for (int i=expo;i!=0;--i){
+		res=res*base;
+	}
+	return res;
+}
