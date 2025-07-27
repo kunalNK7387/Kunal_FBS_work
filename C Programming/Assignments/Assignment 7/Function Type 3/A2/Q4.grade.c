e0ae3cf333a1064567de697975275a4b4d59f300
kunalNK7387 <kunalkhairnar595@gmail.com>
2025-07-27 20:04:50 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignment 7/Function Type 3/A2/Q4.grade.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void grade(int*);
+void main() {
+	int a;
+	printf("Enter marks :");
+	scanf("%d",&a);
+	
+	grade(&a);
+}
+void grade(int* a){
+
+	
+	
+	if(*a>75){
+		printf("Distinction.\n");
+		
+	}else if(*a>65){
+		printf("First Class.\n");
+		
+	}else if(*a>55){
+		printf("Second Class.\n");
+	}else if(*a>=40){
+		printf("Pass class.\n");
+	}else{
+		printf("Fail.\n");
+	}
+}
---
+++ C Programming/Assignments/Assignment 7/Function Type 3/A2/Q6.divisibleby.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void divisible(int*);
+void main() {
+	int no;
+	printf("Enter the number:");
+	scanf("%d",&no);
+
+	divisible(&no);
+}
+void divisible(int* no) {
+
+	
+	if(*no % 3==0 && *no % 5==0) {
+		printf("The number is divisible by both (3 and 5) %d");
+
+	} else if(*no%3==0) {
+		printf("The number is divisible by 3");
+	} else if(*no%5==0) {
+		printf("The number is divisible by 5");
+	} else {
+		printf("The number is not divisible by both");
+	}
+}
